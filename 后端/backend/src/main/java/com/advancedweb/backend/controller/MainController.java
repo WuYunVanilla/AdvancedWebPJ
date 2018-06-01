9d1bad2f5311dd14407770b075931cb9b961731b
RockyLzy <2586365688@qq.com>
2018-06-01 21:48:38 +0800

    update_neo4j_design

--- 后端/backend/src/main/java/com/advancedweb/backend/controller/MainController.java
+++ 后端/backend/src/main/java/com/advancedweb/backend/controller/MainController.java
@@ -27,32 +27,6 @@ public class MainController {
         return courseService.findByCourseId(course_id);
     }
 
-    @RequestMapping(value = "/teach_in", method = RequestMethod.GET)
-    public boolean teach_in(@RequestParam String name, @RequestParam String course_id) {
-        Teacher teacher = teacherService.findByName(name);
-        Course course = courseService.findByCourseId(course_id);
-        if (teacher == null || course == null) {
-            return false;
-        }
-        teacherService.saveTeachIn(name, course_id);
-        return true;
-    }
-
-    @RequestMapping(value = "/save/{course_id}/{mindmap_id}/{json_string}", method = RequestMethod.GET)
-    public Mindmap save(@PathVariable String course_id, @PathVariable String mindmap_id, @PathVariable String json_string) {
-//        System.out.println(course_id + " " + mindmap_id + " " + json_string);
-        Course course = courseService.findByCourseId(course_id);
-        if (course == null) return null;
-
-        Mindmap mindmap = new Mindmap();
-        mindmap.setMindmap_id(mindmap_id);
-        mindmap.setJson_string(json_string);
-        mindmapService.save(mindmap);
-
-        courseService.saveOwn(course_id, mindmap_id);
-        return mindmap;
-    }
-
     @RequestMapping(value = "/test")
     public String test() {
         Teacher teacher = teacherService.findByName("lizongyi");
