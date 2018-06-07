853d7522c3b91a1272f773c0359d947aa3911f48
flower <hn915882174@gmail.com>
2018-06-07 20:58:23 +0800

    delete  onTabChange method

--- 前端/Project/src/app/details/details.component.ts
+++ 前端/Project/src/app/details/details.component.ts
@@ -21,24 +21,6 @@ export class DetailsComponent implements OnInit {
         this.selected_node_id = '';
     }
 
-
-    // public beforeChange($event: NgbTabChangeEvent) {
-    //     if ($event.nextId === 'tab-homework'
-    //         || $event.nextId === 'tab-ppt'
-    //         || $event.nextId === 'tab-resource') {
-    //         // $event.preventDefault();
-    //         this.mindMap.update_selected_knowledge_id();
-    //         const info = this.mindMap.selected_node_id;
-    //         if (info === '') {
-    //             $event.preventDefault();
-    //             alert('请先选择一个知识节点');
-    //         } else {
-    //             this.selected_node_id = info;
-    //             // todo 通过 service 获取实际资源
-    //         }
-    //     }
-    // }
-
     checkTabStatus() {
         this.mindMap.update_selected_knowledge_id();
         this.selected_node_id = this.mindMap.selected_node_id;
